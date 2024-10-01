2832cb1e679fde179ef82e31d656cee33c340b55
shreyashmangale <shreemangale@yahoo.com>
2024-10-01 15:29:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'backgroundImg1': "url('/images/img1.png')"
+    },
+  },
+  plugins: [],
+}
+
